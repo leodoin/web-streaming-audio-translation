62da16cb73ca4009ce816d4d5f49785414c00d50
leodoin <leodoin@gmail.com>
2024-10-23 15:06:22 +0200

    interim refact

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-const sdk = require('microsoft-cognitiveservices-speech-sdk');
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
+import sdk from 'microsoft-cognitiveservices-speech-sdk';
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import fs from 'fs';
+import e from 'express';
 
 const app = express();
 const server = http.createServer(app);
@@ -9,111 +11,133 @@ const io = new Server(server);
 
 app.use(express.static('public'));
 
-const fs = require('fs');
-const path = require('path');
+const recognizer = Recognizer();
 
-
-// Set up environment variables (replace with your own keys if not using env vars)
-const speechKey = process.env.AZURE_SPEECH_KEY || 'your_speech_api_key';
-const serviceRegion = process.env.AZURE_SPEECH_REGION || 'your_region';
-console.log(`Using speech key ${speechKey} in region ${serviceRegion}`);
+server.listen(3000, () => {
+    console.log(`Server listening on http://localhost:3000`);
+});
 
 
 io.on('connection', (socket) => {
-    console.log('Client connected via Socket.io');
+    console.log(`Client connected via Socket.io: ${socket.id}`);
 
-    let {recognizer, pushStream} = setAzureSdk();
-    let fileStream = pcmFileStream('output');
+    socket.on('startStream', () => {
+        console.log('Starting stream...');
+        const rec = recognizer.start({
+            source: 'en-US',
+            target: ['pt', 'fr', 'es', 'de'],
 
+        });
 
-    recognizer.recognizing = (s, e) => {
-        console.log('Interim result:', e.result.text);
-        socket.emit('interimResult', { text: e.result.text });
-    };
-
-    recognizer.recognized = (s, e) => {
-        // Check if the result is a translated speech result
-        if (e.result.reason === sdk.ResultReason.TranslatedSpeech) {
-            console.log(`Final result: ${e.result.text}`);
-            console.log('Translations:', e.result.translations);
-            const translations = {}
-            e.result.translations.privMap.privKeys.forEach((lang, index) => {
-                console.log('Language:', lang);
-                console.log('Index:', index);
-                console.log('Translation:', e.result.translations.privMap.privValues[index]);
-                translations[lang] = e.result.translations.privMap.privValues[index];
-            })
-            console.log({ text: e.result.text, translations });
-            socket.emit('finalResult', { text: e.result.text, translations });
+        rec.onRecognizing = (s,e) => {
+            console.log('Recognizing:', e);
         }
-    };
+        rec.onRecognized = (s,e) => {
+            console.log('Recognized:', e);
+        }
+
+        rec.onCanceled = (s,e) => {
+            console.log('Canceled:', e);
+        }
+    });
+
+    socket.on('audioChunk', (pcmChunk) => {
+        recognizer.push(pcmChunk);
+    });
+
+    socket.on('endOfStream', () => {
+        console.log('End of stream');
+        recognizer.stop();
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected, closing recognizer...');
+        recognizer.stop();
+    });
 
     socket.on('ping', () => {
         console.log('Received ping');
         socket.emit('pong');
     });
 
+});
 
-    socket.on('startStream', () => {
-        console.log('Starting stream...');
+function Recognizer() {
+    // Set up environment variables (replace with your own keys if not using env vars)
+    const speechKey = process.env.AZURE_SPEECH_KEY || 'your_speech_api_key';
+    const serviceRegion = process.env.AZURE_SPEECH_REGION || 'your_region';
+    let recognizer
+    let pushStream
+    let testStream
+
+    const start = ({source, target, onRecognized, onRecognizing}) => {
+        console.log(`translating from ${source} to ${target}`);
+        console.log(`Using speech key ${speechKey} in region ${serviceRegion}`);
+        const translationConfig = sdk.SpeechTranslationConfig.fromSubscription(speechKey, serviceRegion);
+        translationConfig.speechRecognitionLanguage = source ;
+        target.forEach(lang => translationConfig.addTargetLanguage(lang));
+
+        testStream = fs.createWriteStream('test.pcm');
+    
+        pushStream = sdk.AudioInputStream.createPushStream();
+        const audioConfig = sdk.AudioConfig.fromStreamInput(pushStream);
+        recognizer = new sdk.TranslationRecognizer(translationConfig, audioConfig);
+
+        console.log(recognizer)
+
+        recognizer.onRecognized = (s,e) => {
+            console.log('Recognized:', e.result.text);
+            if (e.result.reason === sdk.ResultReason.TranslatedSpeech) {
+                const translations = {}
+                e.result.translations.privMap.privKeys.forEach((lang, index) => {
+                    translations[lang] = e.result.translations.privMap.privValues[index];
+                })
+                onRecognized({ text: e.result.text, translations });
+            }
+        };
+
+        recognizer.onRecognizing = (s,e ) => {
+            console.log('Recognizing:', e.result.text);
+            const translations = {}
+            e.result.translations.privMap.privKeys.forEach((lang, index) => {
+                translations[lang] = e.result.translations.privMap.privValues[index];
+            })
+            onRecognizing({ text: e.result.text, translations });
+        };
 
         recognizer.startContinuousRecognitionAsync(() => {
             console.log('Recognition started...');
         }, (err) => {
-            console.error('Error starting recognition:', err);
-        });
-    })
+            console.log("Error",err)
+        })
 
-    // Handle incoming audio chunks from the client
-    socket.on('audioChunk', (pcmChunk) => {
-        // console.log('Received PCM audio chunk');
+        return recognizer;
+    }
+
+    const push = (pcmChunk) => {
+        if(!pushStream || pushStream.isClosed){ 
+            throw new Error('Stream is closed') 
+        }
+        // console.log('Pushing audio chunk', pcmChunk.length);
         const buffer = Buffer.from(pcmChunk);  // Convert to Node.js Buffer
-        // console.debug('Received audio chunk, size:', buffer.length);
         pushStream.write(buffer);
-        fileStream.write(buffer);
-    });
+        testStream.write(buffer);
+    }
 
-    socket.on('endOfStream', () => {
-        console.log('End of stream');
-        closeStream(pushStream, fileStream, recognizer);
-        // playPCMFile(path.join(__dirname, 'public', 'output.pcm'));
-    });
+    const stop = () => {
+        if (pushStream) pushStream.close();
+        if(testStream) testStream.close();
+        if (recognizer) {
+            recognizer.stopContinuousRecognitionAsync(() => {
+                console.log('Recognition stopped');
+                recognizer.close();
+            });
+        }
+    }
 
-    socket.on('disconnect', () => {
-        console.log('Client disconnected, closing streams...');
-        closeStream(pushStream, fileStream, recognizer);
-    });
-});
-
-function closeStream(pushStream, fileStream, recognizer) {
-    pushStream.close();
-    fileStream.end();
-    recognizer.stopContinuousRecognitionAsync(() => {
-        console.log('Recognition stopped');
-        recognizer.close();
-    });
+    return {
+        start,
+        stop,
+        push
+    }
 }
-
-function pcmFileStream(fileName) {
-    const outputFilePath = path.join(__dirname, 'public',  `${fileName}.pcm`);
-    return fs.createWriteStream(outputFilePath);
-}
-
-function setAzureSdk(){
-    // Set up Azure Speech SDK
-    const translationConfig = sdk.SpeechTranslationConfig.fromSubscription(speechKey, serviceRegion);
-    translationConfig.speechRecognitionLanguage = 'en-US';
-    const translateTo = ['pt', 'fr', 'es', 'de'];
-    translateTo.forEach(lang => translationConfig.addTargetLanguage(lang));
-
-    const pushStream = sdk.AudioInputStream.createPushStream();
-    const audioConfig = sdk.AudioConfig.fromStreamInput(pushStream);
-    const recognizer = new sdk.TranslationRecognizer(translationConfig, audioConfig);
-
-    return {recognizer, pushStream};
-}
-
-// Start the server
-server.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
