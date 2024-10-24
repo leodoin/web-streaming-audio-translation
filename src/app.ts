bfad0b9fc12b51fe8d164c1868e929e06e7173eb
leodoin <leodoin@gmail.com>
2024-10-24 14:42:52 +0200

    Add ioredis and socket.io-redis dependencies

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,22 @@ import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
 import { connectionController } from './socket-controller';
+import { createAdapter } from 'socket.io-redis';
+import { Redis } from 'ioredis';
+
+const pubClient = new Redis({
+    host: process.env.REDISHOST || 'localhost',
+    port: parseInt(process.env.REDISPORT || '6379'),
+  });
+
+const subClient = pubClient.duplicate();
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    adapter: createAdapter({ pubClient, subClient }),
+    cors: { origin: '*'}
+});
 
 app.use(express.static('public'));
 
